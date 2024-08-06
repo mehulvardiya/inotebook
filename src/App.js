60fd2837ff8be1ef7023ac752177e623b0508a23
mehulvardiya <vardiyamehul@gmail.com>
2024-08-06 07:29:41 +0530

    Frontend iNotebook React Project Setup

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       Hello
-      </header>
+      <h1>Hello</h1> 
     </div>
   );
 }
