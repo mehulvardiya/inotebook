96082bfa98a60e028141f8a3065b4bf308118d34
mehulvardiya <vardiyamehul@gmail.com>
2024-08-07 11:14:19 +0530

    useLocation Hook in React in Navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,12 @@
-import React, { useContext, useEffect } from "react";
-import NoteContext from "../context/notes/NoteContext";
+import React from "react";
+// import NoteContext from "../context/notes/NoteContext";
 
 const About = () => {
-  const a = useContext(NoteContext);
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
+  
   return (
     <div>
       <h1>
-        {" "}
-        This is About {a.state.name} and he is class {a.state.class}
+        This is About page
       </h1>
     </div>
   );
