72e3be93ebcf8c1abd591685c7518d247bcce410
mehulvardiya <vardiyamehul@gmail.com>
2024-08-09 08:07:05 +0530

    Adding AddNote component to iNotebook and create AddNote file

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,19 +1,21 @@
 import React, { useContext } from "react";
 import NoteContext from "../context/notes/NoteContext";
 import Noteitem from "./NoteItem";
-
+import AddNote from "./AddNote";
 
 const Notes = () => {
   const context = useContext(NoteContext);
-  const { notes, setNotes } = context;
+  const { notes, addNote } = context;
   return (
-    <div className="row my-3">
-      <h2>Your Note</h2>
-      {notes.map((note) => {
-        return <Noteitem key={note._id} note={note} />;
-
-      })}
-    </div>
+    <>
+      <AddNote />
+      <div className="row my-3">
+        <h2>Your Note</h2>
+        {notes.map((note) => {
+          return <Noteitem key={note._id} note={note} />;
+        })}
+      </div>
+    </>
   );
 };
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -59,8 +59,32 @@ const NoteState = (props) => {
     },
   ];
   const [notes, setNotes] = useState(notesInitial);
+
+  //Add a Note
+  const addNote = (title, description, tag) => {
+    console.log("Adding a new note ");
+    const note = {
+      "_id": "66acb2d3ad093ae701e70b3f",
+      "user": "66ab846397bd4e424d0da9ed",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2024-08-02T10:20:03.365Z",
+      "__v": 0,
+    };
+    setNotes(notes.concat(note));
+  };
+
+  //Delete a Note
+  const deleteNote = () => {
+
+  };
+
+  //Edit a Nots
+  const editNote = () => {};
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
